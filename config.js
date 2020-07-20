020b2b7625ad1a6820362f50bdf6e92553896b54
Akhilkumar31e <akhilkumar31e@gmail.com>
2020-07-20 14:11:09 +0530

    Passport Facebook OAuth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '329055321593538',
+        clientSecret: '4f75990db8594d0ada91e63323e232b6'
+    }
 }
